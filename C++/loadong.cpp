51b794c286a1062f0adf76c13c07c8730353f8aa
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-21 00:58:37 +0800

    update

---
+++ C++/loadong.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        cout <<"|";
+        
+    }
+    
+
+    return 0;
+}
--- c/move.c
+++ c/move.c
@@ -10,8 +10,8 @@ int main()
 		char b = 35;//ASCII of #
 		char c = 37;//ASCII of %
 		char d = 38;//ASCII of &
-		printf("%c %c %c %c" ,a,b,c,d);// 清除畫面
-		system("cls");
+		printf("%c %c %c %c" ,a,b,c,d);
+		system("cls");// 清除畫面
 	}
 	
     return 0;
