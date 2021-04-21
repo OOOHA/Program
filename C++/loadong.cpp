1b7e3dfb15ebc0019e41f2b86b211f8f73d69adb
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-21 19:17:34 +0800

    update

--- C++/loadong.cpp
+++ C++/loadong.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
+#include<windows.h>
 
 using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 100; i++)
+    int count = 0;
+
+    for (int i = 1; i < 10; i++)
     {
         cout <<"|";
+        Sleep(1000);
         
     }
+    /*for (int i = 0; i < 100; i++)
+    {
+        count++;
+        cout << count << "%";
+        system("cls");
+    } */
+    
     
 
     return 0;
