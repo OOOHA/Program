d54fdf31326cd870b8f92fc536327f12af0b7d25
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-21 22:47:02 +0800

    Create testString.java

---
+++ java_code/testString.java
@@ -0,0 +1,12 @@
+package java_code;
+
+public class testString 
+{
+    public static void main(String args[])
+    {
+        String st = "ABC";
+        //st = 123;
+        System.out.println(st);
+    }
+    
+}
