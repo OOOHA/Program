ae222b799c6cab4bb594593700b0055c60633666
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-24 22:36:47 +0800

    spdate

---
+++ c/char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char a = 10;
+    a = a + 10;
+    printf("%d", a);
+
+    return 0;
+}
--- java_code/testString.java
+++ java_code/testString.java
@@ -7,6 +7,9 @@ public class testString
         String st = "ABC";
         //st = 123;
         System.out.println(st);
+
+        char ch = 69;
+        System.out.println(ch);
     }
     
 }
