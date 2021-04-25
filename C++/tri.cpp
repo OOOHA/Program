d7d5c8d8956e08a21bad5c2b9348c6f41fa913de
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-25 19:58:52 +0800

    update

--- C++/tri.cpp
+++ C++/tri.cpp
@@ -4,28 +4,32 @@ using namespace std;
 
 int main()
 {
+    int arr3[5][6] = {};
+    arr3[0][0] = arr3[5][6] = 1;//The numbers of first column are set as 1
 
-int arr3[5][6]={};
-arr3[0][0]=arr3[0][1]=1;//第一列前二個內容為1
-for(int i=1;i<5;i++) 
-{
-    arr3[i][0]=1;
-    for(int j=1;j<=i+1;j++)
+    for(int i = 1; i < 5; i++) 
     {
-        arr3[i][j]=arr3[i-1][j-1]+arr3[i-1][j];
-    }
-}
-for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<6;j++)
+        arr3[i][0] = 1;
+
+        for(int j = 1; j <= i + 1; j++)
         {
-            if(arr3[i][j]!=0)
-            cout<<setw(3)<<arr3[i][j];
+            arr3[i][j] = arr3[i - 1][j - 1] + arr3[i - 1][j];
         }
-        cout<<endl;
     }
 
-    system("pause");
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 6 + 1; j++)
+        {
+            if(arr3[i][j] != 0)
+            {
+                cout << setw(3) << arr3[i][j];
+            }
+        }
+        cout << endl;
+    }
+
+    //system("pause");
 
     return 0;
 }
