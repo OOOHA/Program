a130e7b207576b967aef16fb1792817af1b03444
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-04-25 17:01:15 +0800

    update

---
+++ C++/emptytriandle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << " ";
+        for (int j = 0; j < i; j++)
+        {   
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
