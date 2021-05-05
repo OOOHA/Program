90c2d5924678b6346cc29d453405a26d8a8fe548
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-05-05 12:26:47 +0800

    update

--- c/test_for_c.c
+++ c/test_for_c.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+	int y = 1;
 	
+	printf("%d",a(y));
 	return 0;
+ }
+
+ int a(int *x)
+ {
+	 //printf("%d",x);
+	 return x;
  }
---
+++ java/code/speedChange.java
@@ -0,0 +1,52 @@
+package code;
+
+public class speedChange 
+{
+    float speed = 0;
+    int carNumber;
+    float acceleration;
+    float deceleration;
+    String carColor; 
+
+    public static final String SHOWNUMBER = "car number:";//java rule S1192
+    public static final String SHOWCOLOR = "car color:";//java rule S1192
+
+    public speedChange(int carNumber, float acceleration, float deceleration, String carColor)
+    {
+        this.carNumber = carNumber;
+        this.acceleration = acceleration;
+        this.deceleration = deceleration;
+        this.carColor = carColor;
+    }
+
+    public void carInfo(String sequence, int number, String color)//1st car method
+    {
+        this.carNumber = number;
+        this.carColor = color;
+
+        System.out.println(sequence);
+        System.out.println(SHOWNUMBER + carNumber);
+        System.out.println(SHOWCOLOR + carColor);
+    }
+
+    public float speedChange(float acceleration)////calculate the speed acceleration
+    {
+        speed = speed + acceleration;
+        return speed;
+    }
+
+    public float speedChange(float deceleration, float times)//calculate the speed deceleration
+    {
+        deceleration = deceleration * times;
+
+        if(speed > deceleration)//Because the speed is deceleration not reverse so check if the speed became 0 the speed can't be negative number
+        {
+            speed = speed - deceleration;
+            return speed;
+        }
+        else//if the result become negative number or become 0 then the speed is 0
+        {
+            return 0;
+        }
+    }
+}
