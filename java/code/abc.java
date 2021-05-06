0548b0f42299dcbec325f056f6aacc2e1609db26
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-05-07 00:19:37 +0800

    update

---
+++ java/code/abc.java
@@ -0,0 +1,15 @@
+package code;
+
+public class abc {
+    
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = i; j < i + 1; j++)
+            {
+                System.out.print(j + " ");
+            }
+        }
+    }
+}
