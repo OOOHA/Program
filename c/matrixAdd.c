7d8595b1f7da42c3237379f0637f313815c4e8ee
OOOHA <45551120+OOOHA@users.noreply.github.com>
2022-01-11 01:06:46 +0800

    update

--- c/matrixAdd.c
+++ c/matrixAdd.c
@@ -3,38 +3,41 @@
 #define n 2
 #define p 2
 
-void main()
+int main()
 {
     int A[m + 1][n + 1], B[n + 1][p + 1], C[m + 1][p + 1];
     int i, j, k;
 
-    printf("請依序輸入二維陣列A(3x2)的元素內容:\n");
+    printf("Please enter matrix A(3x2):\n");
     for (i = 1 ; i <= 3; i++) {
         for( j= 1; j <= 2; j++) {
             scanf("%d",&A[i][j]);
         }
-    }/*end for*/
+    }
 
-    printf("請依序輸入二維陣列B(2x2)的元素內容:\n\n");
+    printf("Please enter matrix B(2x2):\n");
     for (i = 1; i <= 2; i++)
     {
         for(j = 1; j <= 2; j++)
         {
             scanf("%d",&B[i][j]);
         }
-    }/*end for*/
+    }
 
+    printf("\nThe result of multiplying two arrays is:\n");
     for (i = 1; i <= m; i++)
     {
         for (j = 1; j <=p ; j++)
         {
-            C[i][j] = 0; /*初始化陣列C */
+            C[i][j] = 0;
             for(k = 1; k <= n; k++)
             {
-                C[i][j] = C[i][j] + A[i][k] * B[k][j]; /*陣列A乘上陣列B,存入陣列C */
+                C[i][j] = C[i][j] + A[i][k] * B[k][j];
             }
-            printf("%d ", C[i][j]); /*輸出陣列C */
+            printf("%d ", C[i][j]);
         }
         printf("\n");
     }
+
+    return 0;
 }
